4d9976d13983f217911fdfdf7686c113fd497f68
Adarshkumar712 <adarshkumar712.ak@gmail.com>
2021-05-24 05:22:55 -0700

    Correction in make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -10,7 +10,7 @@ makedocs(
         "Conditional Random Fields" => "crf.md",
         "ULMFiT" => "ULMFiT.md",
         "Named Entity Recognition" => "ner.md",
-        "Tagging Schemes" => "tagging.md.md",
+        "Tagging Schemes" => "tagging.md",
         "Sentiment Analyzer" => "sentiment.md",
         "API References" => "APIReference.md"
     ],
