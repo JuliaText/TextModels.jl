e620b9b1b73319734da241894bc2c7870dd8e2e6
Roman S Samarev <rssdev10@gmail.com>
2023-11-19 09:33:13 +0300

    tests: decoration of runtests. All the test results in the same table

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,32 @@ using Test
 using TextAnalysis
 using TextModels
 
-println("Running tests:")
+ENV["DATADEPS_ALWAYS_ACCEPT"] = true
 
-include("crf.jl")
-include("ner.jl")
-include("pos.jl")
-include("sentiment.jl")
-include("averagePerceptronTagger.jl")
-include("ulmfit.jl")
+tests = [
+    "crf.jl"
+    "ner.jl"
+    "pos.jl"
+    "sentiment.jl"
+    "averagePerceptronTagger.jl"
+    "ulmfit.jl"
+]
+
+function run_tests()
+    for test in tests
+        @info "Test: $test"
+        Test.@testset verbose = true "\U1F4C2 $test" begin
+            include(test)
+        end
+    end
+end
+
+@static if VERSION >= v"1.7"
+    Test.@testset verbose = true showtiming = true "All tests" begin
+        run_tests()
+    end
+else
+    Test.@testset verbose = true begin
+        run_tests()
+    end
+end
